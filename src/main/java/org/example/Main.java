ba0252afdaf1bf999d9a9cfc7117c4b634357583
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-04-17 09:13:56 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,44 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import java.sql.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+    public static void main(String[] args) {
+
+        String url = "jdbc:oracle:thin:@localhost:1521:xe";
+        String usuario = "RIBERA";
+        String contraseña = "ribera";
+
+        String deptoBuscar = "SISTEMAS";
+
+        String sql = "SELECT e.NOMBRE, e.SALARIO " +
+                "FROM EMPLEADO e " +
+                "JOIN DEPARTAMENTO d ON e.departamento_id = d.id " +
+                "WHERE d.NOMBRE = ?";
+
+        try (Connection conn = DriverManager.getConnection(url, usuario, contraseña);
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, deptoBuscar);
+
+            try (ResultSet rs = ps.executeQuery()) {
+                System.out.println("Empleados del departamento: " + deptoBuscar);
+
+                boolean hayEmpleados = false;
+                while (rs.next()) {
+                    hayEmpleados = true;
+
+                    String nombre = rs.getString("NOMBRE");
+                    double salario = rs.getDouble("SALARIO");
+                    System.out.println("Nombre: " + nombre + " Salario: " + salario + "€");
+                }
+                if (!hayEmpleados) {
+                    System.out.println("No hay empleados .");
+                }
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error: " + e.getMessage());
         }
     }
 }
